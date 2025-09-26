2941d2b1f96f5bfef534504aee5d096fb97f19b8
Pononot <noe.orlando@icloud.com>
2025-09-26 20:19:57 +0200

    added capitalize function and clientId -> clientIds

--- parameters/parameterMaxHandlers.js
+++ parameters/parameterMaxHandlers.js
@@ -1,12 +1,16 @@
 const { PARAMETER_SCHEMA } = require("./parameterRegistry");
 
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 function registerMaxHandlers(maxApi, setParametersForClients) {
   // Auto-generate all Max handlers
   Object.keys(PARAMETER_SCHEMA).forEach((param) => {
-    maxApi.addHandler(`set${capitalize(param)}`, (value, clientId) => {
+    maxApi.addHandler(`set${capitalize(param)}`, (value, clientIds) => {
       return setParametersForClients(
         { [param]: value },
-        clientId ? [clientId] : null,
+        clientIds ? [clientIds] : null,
       );
     });
   });
